a592176043d2f822182f31d439ccaffa1ea3abd9
Silvertapion <Augusto.bonilla@gmail.com>
2016-02-22 10:57:02 -0500

    tercer commit: form_validation_lang

--- application/language/english/form_validation_lang.php
+++ application/language/english/form_validation_lang.php
@@ -8,5 +8,8 @@ $lang['nombre'] = "Por favor, completar el campo Nombre";
 
 $lang['apellidos'] = "Por favor, completar el campo Apellidos";
 
-$lang['dni'] = "Por favor, completar el campo DNI";
+$lang['dni'] = "Por favor, completar el campo Dni";
 
+$lang['fecha_nac'] = "Por favor, completar el campo Fecha_nac";
+$lang['distrito'] = "Por favor, completar el campo Distrito";
+$lang['direccion'] = "Por favor, completar el campo Direccion";
